01d45cb145c4b8ca078f058770c6961dccab9406
Pedro Lamarão <pedro.lamarao@gmail.com>
2026-04-02 00:37:58 -0300

    Benchmark.

--- arithmetics/add_test.go
+++ arithmetics/add_test.go
@@ -4,6 +4,10 @@
 package arithmetics
 
 import (
+	"fmt"
+	"math/big"
+	"math/bits"
+	"math/rand/v2"
 	"slices"
 	"testing"
 
@@ -58,4 +62,47 @@ func TestAdd_ResultLessThanPart_Rapid(t *testing.T) {
 			t.Error("Sum(x,y) < y")
 		}
 	})
+}
+
+func BenchmarkAdd(b *testing.B) {
+	for _, words := range []uint{8, 16, 32, 64, 128, 256} {
+		// generate samples
+		rng := rand.New(rand.NewPCG(31, 39))
+		x := make([]uint, words)
+		for i := range x {
+			x[i] = rng.Uint()
+		}
+		y := make([]uint, words)
+		for i := range y {
+			y[i] = rng.Uint()
+		}
+
+		// translate samples to math/big
+		x_ := big.NewInt(0)
+		for i := len(x); i > 0; i-- {
+			w := big.NewInt(0).SetUint64(uint64(x[i-1]))
+			x_.Lsh(x_, bits.UintSize).Add(x_, w)
+		}
+		y_ := big.NewInt(0)
+		for i := len(y); i > 0; i-- {
+			w := big.NewInt(0).SetUint64(uint64(y[i-1]))
+			y_.Lsh(y_, bits.UintSize).Add(y_, w)
+		}
+
+		// measure purple
+		b.Run(fmt.Sprint("purple", words), func(b *testing.B) {
+			t := make([]uint, words)
+			for b.Loop() {
+				Add(t, x, y)
+			}
+		})
+
+		// measure math/big
+		b.Run(fmt.Sprint("math-big-", words), func(b *testing.B) {
+			t := big.NewInt(0)
+			for b.Loop() {
+				t.Add(x_, y_)
+			}
+		})
+	}
 }
