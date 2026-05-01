55c486af0a0dac9e61742ea85c6add3e20e45b50
Pedro Lamarão <pedro.lamarao@gmail.com>
2026-05-01 11:28:20 -0300

    Harmonizar com algorithms-cxx

--- arithmetics/sum.go
+++ arithmetics/sum.go
@@ -5,35 +5,34 @@ package arithmetics
 
 import "math/bits"
 
-// Sum computes the sum of two integers.
+// Sum of nonnegative integers `x` and `y`.
 //
-// Sum stores into sum the len(sum) least significant words of the result.
-// It permits aliasing sum to x, in which case it becomes "add accumulate".
+// Stores into `r` the `size(r)` least significant words of the result.
+// Permits aliasing `r` to `x`, in which case it "accumulates" the result.
+// Returns the "carry" of the top word of the result.
 //
 // This implementation applies the "school" method described in Knuth, section 4.3.1.
-func Sum(sum, x, y []uint) (carry uint) {
-	sz := len(sum)
+func Sum(r, x, y []uint) (c uint) {
+	rz := len(r)
 	xz := len(x)
 	yz := len(y)
 
-	// count of result words to compute
-	z := min(sz, xz, yz)
+	// count of result words
+	z := min(rz, xz, yz)
 
-	// add x and y word by word,
-	// from least to most significant,
-	// propagating carry
+	// add x and y, word by word, propagating carry
 	for i := 0; i < z; i++ {
-		sum[i], carry = bits.Add(x[i], y[i], carry)
+		r[i], c = bits.Add(x[i], y[i], c)
 	}
 
 	// either propagate carry through x
-	for i := z; i < min(sz, xz); i++ {
-		sum[i], carry = bits.Add(x[i], 0, carry)
+	for i := z; i < min(rz, xz); i++ {
+		r[i], c = bits.Add(x[i], 0, c)
 	}
 
 	// or propagate carry through y
-	for i := z; i < min(sz, yz); i++ {
-		sum[i], carry = bits.Add(0, y[i], carry)
+	for i := z; i < min(rz, yz); i++ {
+		r[i], c = bits.Add(0, y[i], c)
 	}
 
 	return
